78dd48ff4f5f3b401fa9ff64d19b3d47568f0d07
symeon158 <106148298+symeon158@users.noreply.github.com>
2025-11-14 08:35:50 +0200

    Refactor ESG_Analysis.py for improved navigation

--- ESG_Analysis.py
+++ ESG_Analysis.py
@@ -38,7 +38,7 @@ project_7_page = st.Page(
 manpower_info_page = st.Page(
     "views/Manpower Budget Info.py",     # create this file in /views/
     title="Manpower Budget Info",
-    icon="🧍💼",
+    icon="💰",
 )
 
 # --- NAVIGATION SETUP ---
@@ -59,6 +59,7 @@ st.sidebar.markdown(
 
 # --- RUN NAVIGATION ---
 pg.run()
+
 
 
 
